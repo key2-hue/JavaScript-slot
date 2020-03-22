1cc12a2eb44c52a688ae9a1c7e99f3cb1f471991
key2-hue <keymaeda3782@gmail.com>
2020-03-22 13:08:18 +0900

    03/22 13:08

--- index.js
+++ index.js
@@ -1,58 +1,75 @@
 (function() {
-  
-  function startSlot() {
-    // const left = document.getElementById('left');
-    // const center = document.getElementById('center');
-    // const right = document.getElementById('right');
-    x = Math.floor(Math.random() * 3 + 1);
-    y = Math.floor(Math.random() * 3 + 1);
-    z = Math.floor(Math.random() * 3 + 1);
-    console.log(left);
-    switch(x){
-      case 1:
-        console.log("a");
-        left.src = "img/cherry.png";
-        break;
-      case 2:
-        console.log("b");
-        left.src = "img/seven.png";
-        break; 
-      case 3:
-        console.log("c");
-        left.src = "img/watermelon.png";
-        break;
-    } 
-    switch(y){
-      case 1:
-        center.src = "img/cherry.png";
-        break;
-      case 2:
-        center.src = "img/seven.png";
-        break; 
-      case 3:
-        center.src = "img/watermelon.png";
-        break;
-    } 
-    switch(x){
-      case 1:
-        right.src = "img/cherry.png";
-        break;
-      case 2:
-        right.src = "img/seven.png";
-        break; 
-      case 3:
-        right.src = "img/watermelon.png";
-        break;
-    } 
-    let clear = setTimeout(startSlot(), 10000);
+
+  class Slot {
+
+    startSlot() {
+      // const left = document.getElementById('left');
+      // const center = document.getElementById('center');
+      // const right = document.getElementById('right');
+      const x = Math.floor(Math.random() * 3 + 1);
+      const y = Math.floor(Math.random() * 3 + 1);
+      const z = Math.floor(Math.random() * 3 + 1);
+      console.log(x);
+      switch(x){
+        case 1:
+          console.log("a");
+          left.src = "img/cherry.png";
+          break;
+        case 2:
+          console.log("b");
+          left.src = "img/seven.png";
+          break; 
+        case 3:
+          console.log("c");
+          left.src = "img/watermelon.png";
+          break;
+      } 
+      switch(y){
+        case 1:
+          center.src = "img/cherry.png";
+          break;
+        case 2:
+          center.src = "img/seven.png";
+          break; 
+        case 3:
+          center.src = "img/watermelon.png";
+          break;
+      } 
+      switch(z){
+        case 1:
+          right.src = "img/cherry.png";
+          break;
+        case 2:
+          right.src = "img/seven.png";
+          break; 
+        case 3:
+          right.src = "img/watermelon.png";
+          break;
+      } 
+      let clear = setTimeout(startSlot(), 100);
+    }
+    stopSlot() {
+      clearTimeout(clear);
+    }
+    
   }
-  function stopSlot() {
-    clearTimeout(clear);
-  }
+
+  const Slots = [
+    new Slot(),
+    new Slot(),
+    new Slot(),
+  ] 
+
   document.getElementById('start').addEventListener('click',()=>{
-    startSlot();
+    // const left = document.getElementById('left');
+    // console.log(left);
+    Slots.forEach(slot => {
+      slot.startSlot();
+    });
   });
   document.getElementById('stop').addEventListener('click',()=>{
-    
+    clearTimeout(clear);
   });
+  
+  
 })();
