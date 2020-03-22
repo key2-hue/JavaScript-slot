aed2b357ea855be1b5e3bd60c5dc206521b8f58e
key2-hue <keymaeda3782@gmail.com>
2020-03-22 15:03:01 +0900

    03/22 15:02

--- index.js
+++ index.js
@@ -2,25 +2,26 @@
 
   class Slot {
 
+    constructor() {
+      
+    }
+
     startSlot() {
-      // const left = document.getElementById('left');
-      // const center = document.getElementById('center');
-      // const right = document.getElementById('right');
       const x = Math.floor(Math.random() * 3 + 1);
       const y = Math.floor(Math.random() * 3 + 1);
       const z = Math.floor(Math.random() * 3 + 1);
-      console.log(x);
+      // console.log(x);
       switch(x){
         case 1:
-          console.log("a");
+          // console.log("a");
           left.src = "img/cherry.png";
           break;
         case 2:
-          console.log("b");
+          // console.log("b");
           left.src = "img/seven.png";
           break; 
         case 3:
-          console.log("c");
+          // console.log("c");
           left.src = "img/watermelon.png";
           break;
       } 
@@ -50,9 +51,13 @@
         this.startSlot(); }
         , 100);
     }
+
+    
     stopSlot() {
       clearTimeout(this.clear);
     }
+  
+    
     
   }
 
@@ -60,19 +65,41 @@
     new Slot(),
     new Slot(),
     new Slot(),
-  ] 
+  ];
+
+  let slotStart = [];
+
+  
 
   document.getElementById('start').addEventListener('click',()=>{
-    // const left = document.getElementById('left');
-    // console.log(left);
-    Slots.forEach(slot => {
+    Slots.forEach(function(slot, index)  {
       slot.startSlot();
+      console.log(slot);
+      // // slot.classList.add('start');
+      // slotStart.push(slot);
     });
   });
-  const stop = document.querySelector('.stop > input');
-  stop.addEventListener('click',()=>{
-    console.log(stop.data('id'));
+  const stopZero = document.querySelector('.stop0 > input');
+  stopZero.addEventListener('click',()=>{
+    Slots.forEach(function(slot, index)  {
+      slot[0].stopSlot();
+      console.log(slot);
+      // // slot.classList.add('start');
+      // slotStart.push(slot);
+    });
   });
+
+  const stopOne = document.querySelector('.stop1 > input');
+  stopOne.addEventListener('click',()=>{
+    console.log(stopOne.dataset.id);
+  });
+  
+
+  const stopTwo = document.querySelector('.stop2 > input');
+  stopTwo.addEventListener('click',()=>{
+    console.log(stopTwo.dataset.id);
+  });
+  
   
   
 })();
